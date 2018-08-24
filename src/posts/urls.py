eb21ac14a7c93cc739573107a82b76cbf3607c09
Anurag Bisht <bisht.anu05@gmail.com>
2018-08-25 03:19:50 +0400

    CRUD declared with URLs

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls"))
+	url(r'^$', views.post_list),
+    url(r'^create/$', views.post_create),
+    url(r'^update/$', views.post_update),
+    url(r'^detail/$', views.post_detail),
+    url(r'^delete/$', views.post_delete),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def post_create(request, *args, **kwargs):
+	return HttpResponse("<h1>Create</h1>")
+
+def post_detail(request, *args, **kwargs):
+	return HttpResponse("<h1>Detail</h1>")
+
+def post_list(request, *args, **kwargs):
+	return HttpResponse("<h1>List</h1>")
+
+def post_update(request, *args, **kwargs):
+	return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request, *args, **kwargs):
+	return HttpResponse("<h1>Delete</h1>")
