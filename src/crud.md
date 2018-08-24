cf6fc989b6e6dc801bace514b40c7da9e2673ee2
Anurag Bisht <bisht.anu05@gmail.com>
2018-08-25 02:13:16 +0400

    Model added in posts app

---
+++ src/crud.md
@@ -0,0 +1,4 @@
+CREATE    -- Make New     -- POST
+RETRIEVE  -- list/Search  -- GET
+UPDATE    -- Edit -- PUT/PATCH
+DELETE    -- Remove       -- DELETE
---
+++ src/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+# MVC- Model, View, Controller
+
+class Post(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	updated = models.DateTimeField(auto_now = True, auto_now_add = False) #set whenever the fields are updated
+	timestamp = models.DateTimeField(auto_now = False, auto_now_add = True) #set when the field is initialized
+
+	#the difference between python2 and python3 
+	def __unicode__(self):
+		return self.title
+
+	def __str__(self):
+		return self.title
